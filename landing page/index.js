0a6a9ffe128dd55815c5fce4ad8e0ce07e36fb7c
Payel Bera <payel.cs.2024.ipcw@gmail.com>
2025-09-21 23:42:36 +0530

    Added full project files

---
+++ landing page/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const logoVideo = document.getElementById("logoVideo");
+    const animatedLogo = document.getElementById("animatedLogo");
+    const body = document.body;
+
+    logoVideo.addEventListener("ended", () => {
+        // Show the static logo
+        animatedLogo.style.opacity = 1;
+        
+        // Start the animation by adding the new class
+        animatedLogo.classList.add('animate-to-navbar');
+
+        // Hide the video
+        logoVideo.style.opacity = 0;
+
+        // Redirect to home.html after the animation completes
+        setTimeout(() => {
+            // Add a class to fade out the entire page before redirecting
+            body.classList.add('fade-out');
+            
+            setTimeout(() => {
+                window.location.href = "../app/home.html";
+            }, 1000); // Wait for the body fade-out
+        }, 1500); // Wait for the logo animation to finish
+    });
+});
+
+
+
